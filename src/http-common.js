70fb94fadffbddaefd70fb6de2d624b998764605
weber <miidev.weber@gmail.com>
2021-08-17 13:23:17 +0100

    change api url

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://evening-woodland-05630.herokuapp.com//api/",
+  baseURL: "https://evening-woodland-05630.herokuapp.com/api/",
   headers: {
     "Content-type": "application/json"
   }
